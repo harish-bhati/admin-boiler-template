f1ab128a5a356e0272d7c02a00a51a8fb36833fa
Babita Gaur <bgaur@ex2india.com>
2024-09-05 15:11:43 +0530

    icon attributes changed tocamelcase

--- src/assets/icons/SearchIcon.tsx
+++ src/assets/icons/SearchIcon.tsx
@@ -17,7 +17,7 @@ export const SearchIcon = ({
       height={height ? height : '16'}
       fill={fill ? fill : 'none'}
     >
-      <path d="M11.5 21a9.5 9.5 0 1 0 0-19 9.5 9.5 0 0 0 0 19ZM22 22l-2-2" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>
+      <path d="M11.5 21a9.5 9.5 0 1 0 0-19 9.5 9.5 0 0 0 0 19ZM22 22l-2-2" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"></path>
     </svg>
   );
 };
