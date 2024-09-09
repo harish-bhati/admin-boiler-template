a725a30459fdb091bf86f837af5ca183ea7f0f1b
EX2INDIA\bgaur <BABITAGAUR92>
2024-09-09 10:50:01 +0530

    card and other component added

---
+++ src/components/Projects.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+const Projects = () => {
+  return (
+    <div className="shadow-sm rounded-xl border">
+        <h1 className="text-[22px] font-bold border-b px-4 py-3">Projects</h1>
+    </div>
+  );
+};
+
+export default Projects;
