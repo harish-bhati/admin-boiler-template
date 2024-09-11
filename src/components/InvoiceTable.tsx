860a9d5cd76e8858e50c35e683853603b3043760
Babita Gaur <bgaur@EX2INDIA.LOCAL>
2024-09-11 15:50:07 +0530

    feature/invoicepage

---
+++ src/components/InvoiceTable.tsx
@@ -0,0 +1,179 @@
+import React from "react";
+
+
+const InvoiceTable = () => {
+  return (
+  <div className="table px-4 py-5 w-full">
+    <table className="w-full border rounded-xl">
+        <thead>
+        <tr className="text-left border-b rounded-xl">
+                <th className="p-2">Invoice Number</th>
+                <th>Client Name</th>
+                <th>Role</th>
+                <th>Status</th>
+                <th>Contact Number</th>
+                <th>Actions</th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr className="text-left border-b text-[14px]">
+                <td className="p-2">TIL-001</td>
+                <td>ACME Construction Ltd</td>
+                <td>Electrician</td>
+                <td>Permanent</td>
+                <td>999-999-999</td>
+                <td className="list-none flex gap-2 justify-start align-middle mt-1">
+                    <li><a href="">Edit</a></li>
+                    <li><a href="">Delete</a></li>
+                </td>
+        </tr>
+        <tr className="text-left border-b text-[14px]">
+                <td className="p-2">TIL-001</td>
+                <td>ACME Construction Ltd</td>
+                <td>Electrician</td>
+                <td>Permanent</td>
+                <td>999-999-999</td>
+                <td className="list-none flex gap-2 justify-start align-middle mt-1">
+                    <li><a href="">Edit</a></li>
+                    <li><a href="">Delete</a></li>
+                </td>
+        </tr>
+        <tr className="text-left border-b text-[14px]">
+                <td className="p-2">TIL-001</td>
+                <td>ACME Construction Ltd</td>
+                <td>Electrician</td>
+                <td>Permanent</td>
+                <td>999-999-999</td>
+                <td className="list-none flex gap-2 justify-start align-middle mt-1">
+                    <li><a href="">Edit</a></li>
+                    <li><a href="">Delete</a></li>
+                </td>
+        </tr>
+        <tr className="text-left border-b text-[14px]">
+                <td className="p-2">TIL-001</td>
+                <td>ACME Construction Ltd</td>
+                <td>Electrician</td>
+                <td>Permanent</td>
+                <td>999-999-999</td>
+                <td className="list-none flex gap-2 justify-start align-middle mt-1">
+                    <li><a href="">Edit</a></li>
+                    <li><a href="">Delete</a></li>
+                </td>
+        </tr>
+        <tr className="text-left border-b text-[14px]">
+                <td className="p-2">TIL-001</td>
+                <td>ACME Construction Ltd</td>
+                <td>Electrician</td>
+                <td>Permanent</td>
+                <td>999-999-999</td>
+                <td className="list-none flex gap-2 justify-start align-middle mt-1">
+                    <li><a href="">Edit</a></li>
+                    <li><a href="">Delete</a></li>
+                </td>
+        </tr>
+        <tr className="text-left border-b text-[14px]">
+                <td className="p-2">TIL-001</td>
+                <td>ACME Construction Ltd</td>
+                <td>Electrician</td>
+                <td>Permanent</td>
+                <td>999-999-999</td>
+                <td className="list-none flex gap-2 justify-start align-middle mt-1">
+                    <li><a href="">Edit</a></li>
+                    <li><a href="">Delete</a></li>
+                </td>
+        </tr>
+        <tr className="text-left border-b text-[14px]">
+                <td className="p-2">TIL-001</td>
+                <td>ACME Construction Ltd</td>
+                <td>Electrician</td>
+                <td>Permanent</td>
+                <td>999-999-999</td>
+                <td className="list-none flex gap-2 justify-start align-middle mt-1">
+                    <li><a href="">Edit</a></li>
+                    <li><a href="">Delete</a></li>
+                </td>
+        </tr>
+        <tr className="text-left border-b text-[14px]">
+                <td className="p-2">TIL-001</td>
+                <td>ACME Construction Ltd</td>
+                <td>Electrician</td>
+                <td>Permanent</td>
+                <td>999-999-999</td>
+                <td className="list-none flex gap-2 justify-start align-middle mt-1">
+                    <li><a href="">Edit</a></li>
+                    <li><a href="">Delete</a></li>
+                </td>
+        </tr>
+        <tr className="text-left border-b text-[14px]">
+                <td className="p-2">TIL-001</td>
+                <td>ACME Construction Ltd</td>
+                <td>Electrician</td>
+                <td>Permanent</td>
+                <td>999-999-999</td>
+                <td className="list-none flex gap-2 justify-start align-middle mt-1">
+                    <li><a href="">Edit</a></li>
+                    <li><a href="">Delete</a></li>
+                </td>
+        </tr>
+        <tr className="text-left border-b text-[14px]">
+                <td className="p-2">TIL-001</td>
+                <td>ACME Construction Ltd</td>
+                <td>Electrician</td>
+                <td>Permanent</td>
+                <td>999-999-999</td>
+                <td className="list-none flex gap-2 justify-start align-middle mt-1">
+                    <li><a href="">Edit</a></li>
+                    <li><a href="">Delete</a></li>
+                </td>
+        </tr>
+        <tr className="text-left border-b text-[14px]">
+                <td className="p-2">TIL-001</td>
+                <td>ACME Construction Ltd</td>
+                <td>Electrician</td>
+                <td>Permanent</td>
+                <td>999-999-999</td>
+                <td className="list-none flex gap-2 justify-start align-middle mt-1">
+                    <li><a href="">Edit</a></li>
+                    <li><a href="">Delete</a></li>
+                </td>
+        </tr>
+        <tr className="text-left border-b text-[14px]">
+                <td className="p-2">TIL-001</td>
+                <td>ACME Construction Ltd</td>
+                <td>Electrician</td>
+                <td>Permanent</td>
+                <td>999-999-999</td>
+                <td className="list-none flex gap-2 justify-start align-middle mt-1">
+                    <li><a href="">Edit</a></li>
+                    <li><a href="">Delete</a></li>
+                </td>
+        </tr>
+        <tr className="text-left border-b text-[14px]">
+                <td className="p-2">TIL-001</td>
+                <td>ACME Construction Ltd</td>
+                <td>Electrician</td>
+                <td>Permanent</td>
+                <td>999-999-999</td>
+                <td className="list-none flex gap-2 justify-start align-middle mt-1">
+                    <li><a href="">Edit</a></li>
+                    <li><a href="">Delete</a></li>
+                </td>
+        </tr>
+        <tr className="text-left border-b text-[14px]">
+                <td className="p-2">TIL-001</td>
+                <td>ACME Construction Ltd</td>
+                <td>Electrician</td>
+                <td>Permanent</td>
+                <td>999-999-999</td>
+                <td className="list-none flex gap-2 justify-start align-middle mt-1">
+                    <li><a href="">Edit</a></li>
+                    <li><a href="">Delete</a></li>
+                </td>
+        </tr>
+        </tbody>
+    </table>
+  </div>
+  );
+};
+
+export default InvoiceTable;
