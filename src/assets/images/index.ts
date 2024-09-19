52bf365fb2fb129c921937a7c3b5f94d06d3731c
Babita Gaur <bgaur@EX2INDIA.LOCAL>
2024-09-19 11:46:14 +0530

    color fonts and banner added

--- src/assets/images/index.ts
+++ src/assets/images/index.ts
@@ -1,9 +1,11 @@
 import calender from './Calender.png';
 import projects from './Projects.png';
 import workers from './Worker.png';
+import WelcomeBanner from './WelcomeBanner.png';
 
 export {
   calender,
   projects,
-  workers
+  workers,
+  WelcomeBanner
 }
