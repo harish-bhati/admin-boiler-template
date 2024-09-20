b1d4a4b26f3efceda97324964240d37419ea98fc
EX2INDIA\bgaur <BABITAGAUR92>
2024-09-20 10:42:43 +0530

    card design updated

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -4,15 +4,17 @@ import { Graph1 } from "../assets/images";
 
 const Card = () => {
   return (
-    <div className="card-container grid grid-cols-3 gap-3">
+    <div className="card-container grid grid-cols-2 gap-3">
       <div className="shadow-sm rounded-xl px-4 py-3 min-w-[150px] border ">
-        <div className="flex items-center gap-2 mb-4">
-          <div className="bg-blue-verylight h-10 w-10 inline-flex justify-center items-center rounded-xl">
-            <DataIcon />
+        <div className="flex items-center gap-2 mb-4 justify-between">
+          <div className="flex items-center gap-2">
+            <div className="bg-blue-verylight h-10 w-10 inline-flex justify-center items-center rounded-xl">
+              <DataIcon />
+            </div>
+            <p className="text-[14px] text-black-dark font-latoB ">
+              Total Projects
+            </p>
           </div>
-          <p className="text-[14px] text-black-dark font-latoB ">
-            Total Projects
-          </p>
           <a className="bg-blue-verylight h-10 w-10 inline-flex justify-center items-center rounded-xl cursor-pointer">
             <MenuDropdown />
           </a>
@@ -26,13 +28,15 @@ const Card = () => {
         </div>
       </div>
       <div className="shadow-sm rounded-xl px-4 py-3 min-w-[150px] border ">
-        <div className="flex items-center gap-2 mb-4">
-          <div className="bg-blue-verylight h-10 w-10 inline-flex justify-center items-center rounded-xl">
-            <DataIcon />
+        <div className="flex items-center gap-2 mb-4 justify-between">
+          <div className="flex items-center gap-2">
+            <div className="bg-blue-verylight h-10 w-10 inline-flex justify-center items-center rounded-xl">
+              <DataIcon />
+            </div>
+            <p className="text-[14px] text-black-dark font-latoB ">
+              Total Projects
+            </p>
           </div>
-          <p className="text-[14px] text-black-dark font-latoB ">
-            Total Projects
-          </p>
           <a className="bg-blue-verylight h-10 w-10 inline-flex justify-center items-center rounded-xl cursor-pointer">
             <MenuDropdown />
           </a>
@@ -46,13 +50,15 @@ const Card = () => {
         </div>
       </div>
       <div className="shadow-sm rounded-xl px-4 py-3 min-w-[150px] border ">
-        <div className="flex items-center gap-2 mb-4">
-          <div className="bg-blue-verylight h-10 w-10 inline-flex justify-center items-center rounded-xl">
-            <DataIcon />
+        <div className="flex items-center gap-2 mb-4 justify-between">
+          <div className="flex items-center gap-2">
+            <div className="bg-blue-verylight h-10 w-10 inline-flex justify-center items-center rounded-xl">
+              <DataIcon />
+            </div>
+            <p className="text-[14px] text-black-dark font-latoB ">
+              Total Projects
+            </p>
           </div>
-          <p className="text-[14px] text-black-dark font-latoB ">
-            Total Projects
-          </p>
           <a className="bg-blue-verylight h-10 w-10 inline-flex justify-center items-center rounded-xl cursor-pointer">
             <MenuDropdown />
           </a>
@@ -66,53 +72,15 @@ const Card = () => {
         </div>
       </div>
       <div className="shadow-sm rounded-xl px-4 py-3 min-w-[150px] border ">
-        <div className="flex items-center gap-2 mb-4">
-          <div className="bg-blue-verylight h-10 w-10 inline-flex justify-center items-center rounded-xl">
-            <DataIcon />
+        <div className="flex items-center gap-2 mb-4 justify-between">
+          <div className="flex items-center gap-2">
+            <div className="bg-blue-verylight h-10 w-10 inline-flex justify-center items-center rounded-xl">
+              <DataIcon />
+            </div>
+            <p className="text-[14px] text-black-dark font-latoB ">
+              Total Projects
+            </p>
           </div>
-          <p className="text-[14px] text-black-dark font-latoB ">
-            Total Projects
-          </p>
-          <a className="bg-blue-verylight h-10 w-10 inline-flex justify-center items-center rounded-xl cursor-pointer">
-            <MenuDropdown />
-          </a>
-        </div>
-        <div className="flex justify-between bg-slate-50 p-3 rounded-xl gap-[5px] items-center">
-          <img className="w-[100px] h-[50px]" src={Graph1} alt="Graph1" />
-          <div className="flex flex-col items-center">
-            <p className="text-[16px] font-latoB text-blue-dark ">$3,020</p>
-            <p className="text-[12px] font-latoB"> 30.6%</p>
-          </div>
-        </div>
-      </div>
-      <div className="shadow-sm rounded-xl px-4 py-3 min-w-[150px] border ">
-        <div className="flex items-center gap-2 mb-4">
-          <div className="bg-blue-verylight h-10 w-10 inline-flex justify-center items-center rounded-xl">
-            <DataIcon />
-          </div>
-          <p className="text-[14px] text-black-dark font-latoB ">
-            Total Projects
-          </p>
-          <a className="bg-blue-verylight h-10 w-10 inline-flex justify-center items-center rounded-xl cursor-pointer">
-            <MenuDropdown />
-          </a>
-        </div>
-        <div className="flex justify-between bg-slate-50 p-3 rounded-xl gap-[5px] items-center">
-          <img className="w-[100px] h-[50px]" src={Graph1} alt="Graph1" />
-          <div className="flex flex-col items-center">
-            <p className="text-[16px] font-latoB text-blue-dark ">$3,020</p>
-            <p className="text-[12px] font-latoB"> 30.6%</p>
-          </div>
-        </div>
-      </div>
-      <div className="shadow-sm rounded-xl px-4 py-3 min-w-[150px] border ">
-        <div className="flex items-center gap-2 mb-4">
-          <div className="bg-blue-verylight h-10 w-10 inline-flex justify-center items-center rounded-xl">
-            <DataIcon />
-          </div>
-          <p className="text-[14px] text-black-dark font-latoB ">
-            Total Projects
-          </p>
           <a className="bg-blue-verylight h-10 w-10 inline-flex justify-center items-center rounded-xl cursor-pointer">
             <MenuDropdown />
           </a>
