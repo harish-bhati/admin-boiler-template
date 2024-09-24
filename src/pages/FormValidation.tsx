9a8278f719e8af97ec7a39421c4e5f2692a7faf7
Babita Gaur <bgaur@ex2india.com>
2024-09-24 14:20:56 +0530

    npm install run

---
+++ src/pages/FormValidation.tsx
@@ -0,0 +1,18 @@
+import Header from "../components/Header";
+import Sidebar from "../components/Sidebar";
+import Iframe from "../components/Iframe";
+import Table from "../components/Table";
+import CardList from "../components/CardList";
+// import Form from "../components/Form";
+const FormValidation = () => {
+  return (
+    <>
+      {/* <section>
+       <Form/>
+       
+      </section> */}
+    </>
+  );
+};
+
+export default FormValidation;
